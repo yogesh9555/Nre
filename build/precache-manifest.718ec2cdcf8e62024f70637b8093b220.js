8ea3ce7264e288c793e4f09a30ea5b749588aff0
yogesh9555 <71551746+yogesh9555@users.noreply.github.com>
2020-12-13 15:11:30 +0530

    Add files via upload

---
+++ build/precache-manifest.718ec2cdcf8e62024f70637b8093b220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddc5703efb2bc1535cda189ff53be85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "386acb1c8e31cb531437",
+    "url": "/static/css/main.24b95877.chunk.css"
+  },
+  {
+    "revision": "449e67e5cf595cf90622",
+    "url": "/static/js/2.9ef19d3e.chunk.js"
+  },
+  {
+    "revision": "386acb1c8e31cb531437",
+    "url": "/static/js/main.fb5615c3.chunk.js"
+  },
+  {
+    "revision": "2bde375618577b953c84",
+    "url": "/static/js/runtime~main.cc205996.js"
+  },
+  {
+    "revision": "27e6f3bf5eecfe8fdb6d5064e2b78fa3",
+    "url": "/static/media/Image71.27e6f3bf.jpg"
+  },
+  {
+    "revision": "80b629d06fbdc86af404df6a6ceee2f8",
+    "url": "/static/media/Mission2.80b629d0.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  }
+]);
